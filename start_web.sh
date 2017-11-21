d97d2abeb358a56e3a5b4bc28156a40e5feab253
tshr20140816 <tshr20140816@yahoo.co.jp>
2017-11-21 09:17:33 +0900

    Update start_web.sh

--- start_web.sh
+++ start_web.sh
@@ -15,4 +15,6 @@ if [ ! -v LOGGLY_TOKEN ]; then
   exit
 fi
 
+curl -H "content-type:text/plain" -d 'INSTANCE START' http://logs-01.loggly.com/inputs/${LOGGLY_TOKEN}/tag/hp/
+
 vendor/bin/heroku-php-apache2 -C apache.conf www
