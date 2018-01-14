64d14507e561810c85a0ab134fd7a75e2d12bcba
tshr20140816 <tshr20140816@yahoo.co.jp>
2018-01-14 18:26:57 +0900

    Update index.php

--- www/index.php
+++ www/index.php
@@ -4,7 +4,7 @@ $pid = getmypid();
 
 $url = 'http://freegeoip.net/json/' . $_SERVER['HTTP_X_FORWARDED_FOR'];
 
-$json = json_decode(file_get_contents($url);, true);
+$json = json_decode(file_get_contents($url), true);
 
 error_log("${pid} START " . $_SERVER['HTTP_X_FORWARDED_FOR'] . ' ' . $json['country_name'] . ' ' . $_SERVER['HTTP_USER_AGENT']);
 
