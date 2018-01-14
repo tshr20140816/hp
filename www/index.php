eb0fb90efac871fbbf628e6ac3cc2ba3e4725ff2
tshr20140816 <tshr20140816@yahoo.co.jp>
2018-01-14 18:16:24 +0900

    Update index.php

--- www/index.php
+++ www/index.php
@@ -4,6 +4,10 @@ $pid = getmypid();
 
 error_log("${pid} START " . $_SERVER['HTTP_X_FORWARDED_FOR'] . ' ' . $_SERVER['HTTP_USER_AGENT']);
 
+$url = 'http://freegeoip.net/xml/' . $_SERVER['HTTP_X_FORWARDED_FOR'];
+
+$xml = file_get_contents($url);
+
 sleep(25);
 
 error_log("${pid} FINISH ");
